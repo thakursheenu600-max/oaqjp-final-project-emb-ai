c9a0ce5c97054979499778c5437fae5cd9e5e43b
thakursheenu600-max <yourgithubemail@example.com>
2026-05-10 10:19:08 -0400

    Added error handling

--- server.py
+++ server.py
@@ -6,13 +6,15 @@ app = Flask("Emotion Detector")
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
-
 @app.route("/emotionDetector")
 def emotion_detector_route():
 
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
+
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
 
     formatted_response = (
         f"For the given statement, the system response is "
@@ -25,6 +27,5 @@ def emotion_detector_route():
     )
 
     return formatted_response
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
